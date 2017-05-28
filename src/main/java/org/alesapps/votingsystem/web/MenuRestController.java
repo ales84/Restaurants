dd6e316363de9e4f2922ddeeded5ff6d85eca6c3
ales84 <tolyaskrg@gmail.com>
2017-05-28 12:02:23 +0600

    Refactoring

--- src/main/java/org/alesapps/votingsystem/web/MenuRestController.java
+++ src/main/java/org/alesapps/votingsystem/web/MenuRestController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.alesapps.votingsystem.web.MenuRestController.REST_URL;
+
 /**
  * Created by Anatoliy Kozhayev on 08.05.2017.
  */
 @RestController
-@RequestMapping("/menus")
+@RequestMapping(REST_URL)
 public class MenuRestController extends RootController {
+    static final String REST_URL = "/api/v1/menus";
 
     private MenuService menuService;
     private DishService dishService;
@@ -27,10 +30,26 @@ public class MenuRestController extends RootController {
         this.dishService = dishService;
     }
 
-    @JsonView(View.Summary.class)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Menu> getMenus() {
         return menuService.getAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Menu createMenu(@RequestBody Menu menu) {
+        menu.setId(null);
+        return menuService.create(menu,menu.getRestaurant().getId());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Menu> updateMenus(@RequestBody List<Menu> menus) {
+        menus.forEach(menu -> menuService.update(menu,menu.getRestaurant().getId()));
+        return menus;
+    }
+
+    @DeleteMapping
+    public void deleteMenus() {
+        menuService.deleteAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -38,10 +57,14 @@ public class MenuRestController extends RootController {
         return menuService.get(id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Menu createMenu(@RequestBody Menu menu) {
-        menu.setId(null);
-        return menu;/*??????????*/
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Menu updateMenu(@PathVariable("id") Integer id, @RequestBody Menu menu) {
+        menu.setId(id);
+        return menuService.update(menu,menu.getRestaurant().getId());
     }
 
+    @DeleteMapping(value = "/{id}")
+    public void deleteMenu(@PathVariable("id") Integer id) {
+        menuService.delete(id);
+    }
 }
