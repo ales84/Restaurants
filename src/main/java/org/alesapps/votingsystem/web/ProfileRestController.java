a1d42015d7b8a4ba4c94486422695a997ddac2c4
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:31:20 +0600

    Initial

---
+++ src/main/java/org/alesapps/votingsystem/web/ProfileRestController.java
@@ -0,0 +1,51 @@
+package org.alesapps.votingsystem.web;
+
+import org.alesapps.votingsystem.AuthorizedUser;
+import org.alesapps.votingsystem.model.User;
+import org.alesapps.votingsystem.service.UserService;
+import org.alesapps.votingsystem.to.UserTo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import static org.alesapps.votingsystem.web.ProfileRestController.REST_URL;
+
+/**
+ * Created by Anatoliy Kozhayev on 12.06.2017.
+ */
+@RestController
+@RequestMapping(REST_URL)
+public class ProfileRestController extends RootController {
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    static final String REST_URL = "/api/v1/profile";
+
+    private UserService userService;
+
+    @Autowired
+    public ProfileRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@AuthenticationPrincipal AuthorizedUser authorizedUser) {
+        LOG.info("get user {}", authorizedUser.getId());
+        return userService.get(authorizedUser.getId());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody UserTo userTo, @AuthenticationPrincipal AuthorizedUser authorizedUser) {
+        userTo.setId(authorizedUser.getId());
+        LOG.info("update {} with id={}", userTo, authorizedUser.getId());
+        userService.update(userTo);
+    }
+
+    @DeleteMapping()
+    public void delete(@AuthenticationPrincipal AuthorizedUser authorizedUser) {
+        LOG.info("delete user {}", authorizedUser.getId());
+        userService.delete(authorizedUser.getId());
+    }
+}
