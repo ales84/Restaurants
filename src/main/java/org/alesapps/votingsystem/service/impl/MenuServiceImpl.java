386633144f7e39400f15de7ff52d25508126c8de
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:34:12 +0600

    Added ByDate methods

--- src/main/java/org/alesapps/votingsystem/service/impl/MenuServiceImpl.java
+++ src/main/java/org/alesapps/votingsystem/service/impl/MenuServiceImpl.java
@@ -1,6 +1,8 @@
 package org.alesapps.votingsystem.service.impl;
 
+import org.alesapps.votingsystem.model.Dish;
 import org.alesapps.votingsystem.model.Menu;
+import org.alesapps.votingsystem.repository.DishRepository;
 import org.alesapps.votingsystem.repository.MenuRepository;
 import org.alesapps.votingsystem.service.MenuService;
 import org.alesapps.votingsystem.util.ValidationUtil;
@@ -9,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Anatoliy Kozhayev on 26.04.2017.
@@ -19,31 +23,59 @@ public class MenuServiceImpl implements MenuService {
 
     private MenuRepository menuRepository;
 
+    private DishRepository dishRepository;
+
     @Autowired
-    public MenuServiceImpl(MenuRepository menuRepository) {
+    public MenuServiceImpl(MenuRepository menuRepository, DishRepository dishRepository) {
         this.menuRepository = menuRepository;
+        this.dishRepository = dishRepository;
     }
 
     @Override
     public Menu create(Menu menu, int restaurantId) {
         Assert.notNull(menu, "menu must not be null");
-        return menuRepository.save(menu, restaurantId);
+
+        Menu created = menuRepository.save(menu, restaurantId);
+
+        Set<Dish> menuDishes = created.getDishes();
+        if (menuDishes != null) {
+            menuDishes.forEach(dish -> {
+                dish.setId(null);
+                dishRepository.save(dish, created.getId());
+            });
+        }
+
+        return created;
     }
 
     @Override
-    public Menu get(int id) throws NotFoundException {
-        return ValidationUtil.checkNotFoundWithId(menuRepository.get(id), id);
+    public Menu get(int id, int restaurantId) throws NotFoundException {
+        return ValidationUtil.checkNotFoundWithId(menuRepository.get(id, restaurantId), id);
     }
 
     @Override
     public Menu update(Menu menu, int restaurantId) {
         Assert.notNull(menu, "menu must not be null");
+
+        Set<Dish> oldMenuDishes = get(menu.getId(), restaurantId).getDishes();
+        if (oldMenuDishes != null) {
+            oldMenuDishes.forEach(dish -> dishRepository.delete(dish.getId(), menu.getId()));
+        }
+
+        Set<Dish> newMenuDishes = menu.getDishes();
+        if (newMenuDishes != null) {
+            newMenuDishes.forEach(dish -> {
+                dish.setId(null);
+                dishRepository.save(dish, menu.getId());
+            });
+        }
+
         return menuRepository.save(menu, restaurantId);
     }
 
     @Override
-    public void delete(int id) throws NotFoundException {
-        ValidationUtil.checkNotFoundWithId(menuRepository.delete(id), id);
+    public void delete(int id, int restaurantId) throws NotFoundException {
+        ValidationUtil.checkNotFoundWithId(menuRepository.delete(id, restaurantId), id);
     }
 
     @Override
@@ -54,5 +86,15 @@ public class MenuServiceImpl implements MenuService {
     @Override
     public List<Menu> getAll() {
         return menuRepository.getAll();
+    }
+
+    @Override
+    public List<Menu> getAllByDate(LocalDate date) {
+        return menuRepository.getAllByDate(date);
+    }
+
+    @Override
+    public List<Menu> getAllByDate(LocalDate date, int restaurantId) {
+        return menuRepository.getAllByDate(date, restaurantId);
     }
 }
