f2b5024eeb3b7b36bf9b36eb43739d17ad2ebcdc
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:34:00 +0600

    Added ByDate methods

--- src/main/java/org/alesapps/votingsystem/repository/impl/MenuRepositoryImpl.java
+++ src/main/java/org/alesapps/votingsystem/repository/impl/MenuRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -34,8 +35,11 @@ public class MenuRepositoryImpl implements MenuRepository {
 
     @Transactional
     @Override
-    public boolean delete(int id) {
-        return em.createNamedQuery(Menu.DELETE).setParameter("id", id).executeUpdate() != 0;
+    public boolean delete(int id, int restaurantId) {
+        return em.createNamedQuery(Menu.DELETE)
+                .setParameter("id", id)
+                .setParameter("restaurantId", restaurantId)
+                .executeUpdate() != 0;
     }
 
     @Transactional
@@ -45,12 +49,28 @@ public class MenuRepositoryImpl implements MenuRepository {
     }
 
     @Override
-    public Menu get(int id) {
-        return em.find(Menu.class, id);
+    public Menu get(int id, int restaurantId) {
+        Menu menu = em.find(Menu.class, id);
+        return menu != null && menu.getRestaurant().getId() == restaurantId ? menu : null;
     }
 
     @Override
     public List<Menu> getAll() {
-        return em.createNamedQuery(Menu.ALL, Menu.class).getResultList();
+        return em.createNamedQuery(Menu.GET_ALL, Menu.class).getResultList();
+    }
+
+    @Override
+    public List<Menu> getAllByDate(LocalDate date) {
+        return em.createNamedQuery(Menu.GET_ALL_BY_DATE, Menu.class)
+                .setParameter("date", date)
+                .getResultList();
+    }
+
+    @Override
+    public List<Menu> getAllByDate(LocalDate date, int restaurantId) {
+        return em.createNamedQuery(Menu.GET_ALL_BY_DATE_AND_RESTAURANT, Menu.class)
+                .setParameter("date", date)
+                .setParameter("restaurantId", restaurantId)
+                .getResultList();
     }
 }
