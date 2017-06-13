b247920df3dd2bcec8aad5053559c79a0fdc0ac8
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:34:07 +0600

    Added ByDate methods

--- src/main/java/org/alesapps/votingsystem/service/MenuService.java
+++ src/main/java/org/alesapps/votingsystem/service/MenuService.java
@@ -3,6 +3,7 @@ package org.alesapps.votingsystem.service;
 import org.alesapps.votingsystem.model.Menu;
 import org.alesapps.votingsystem.util.exception.NotFoundException;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -12,13 +13,17 @@ public interface MenuService {
 
     Menu create(Menu menu, int restaurantId);
 
-    Menu get(int id) throws NotFoundException;
+    Menu get(int id, int restaurantId) throws NotFoundException;
 
     Menu update(Menu menu, int restaurantId);
 
-    void delete(int id) throws NotFoundException;
+    void delete(int id, int restaurantId) throws NotFoundException;
 
     void deleteAll();
 
     List<Menu> getAll();
+
+    List<Menu> getAllByDate(LocalDate date);
+
+    List<Menu> getAllByDate(LocalDate date, int restaurantId);
 }
