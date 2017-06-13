35a85632caf8d9a996d06e2ed3d8aab4ebf2484c
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:32:01 +0600

    Added GrantedAuthority as parent

--- src/main/java/org/alesapps/votingsystem/model/Role.java
+++ src/main/java/org/alesapps/votingsystem/model/Role.java
@@ -1,9 +1,16 @@
 package org.alesapps.votingsystem.model;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * Created by Anatoliy Kozhayev on 17.04.2017.
  */
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
