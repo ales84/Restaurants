d8ae06224b16415c882f625a98b4fa34ee5c3168
ales84 <tolyaskrg@gmail.com>
2017-06-13 10:33:00 +0600

    Refactoring

--- src/main/java/org/alesapps/votingsystem/model/Restaurant.java
+++ src/main/java/org/alesapps/votingsystem/model/Restaurant.java
@@ -1,13 +1,8 @@
 package org.alesapps.votingsystem.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonView;
-import org.alesapps.votingsystem.json.View;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
-import java.util.Set;
 
 /**
  * Created by Anatoliy Kozhayev on 17.04.2017.
@@ -15,7 +10,7 @@ import java.util.Set;
 @NamedQueries({
         @NamedQuery(name = Restaurant.DELETE, query = "DELETE FROM Restaurant r WHERE r.id=:id"),
         @NamedQuery(name = Restaurant.DELETE_ALL, query = "DELETE FROM Restaurant r"),
-        @NamedQuery(name = Restaurant.ALL, query = "SELECT r FROM Restaurant r")
+        @NamedQuery(name = Restaurant.GET_ALL, query = "SELECT r FROM Restaurant r")
 })
 @Entity
 @Table(name = "restaurants", uniqueConstraints = {@UniqueConstraint(name = "restaurants_name_idx", columnNames = "name")})
@@ -23,9 +18,8 @@ public class Restaurant extends BaseEntity {
 
     public static final String DELETE = "Restaurant.delete";
     public static final String DELETE_ALL = "Restaurant.deleteAll";
-    public static final String ALL = "Restaurant.getAll";
+    public static final String GET_ALL = "Restaurant.getAll";
 
-    @JsonView(View.Summary.class)
     @NotBlank
     @Column(name = "name", unique = true, nullable = false)
     private String name;
@@ -49,8 +43,8 @@ public class Restaurant extends BaseEntity {
     @Override
     public String toString() {
         return "Restaurant{" +
-                "id='" + getId() +
-                "name='" + name +
+                "id=" + getId() +
+                ", name=" + name +
                 '}';
     }
 }
