9b296eaaaf27b2bafb955e1b1271053b95b1f5fd
ales84 <tolyaskrg@gmail.com>
2017-06-16 16:59:56 +0600

    Added started sequence

--- src/main/java/org/alesapps/votingsystem/model/BaseEntity.java
+++ src/main/java/org/alesapps/votingsystem/model/BaseEntity.java
@@ -10,9 +10,10 @@ import javax.persistence.*;
 @MappedSuperclass
 @Access(AccessType.FIELD)
 public class BaseEntity {
+    public static final int START_SEQ = 1;
 
     @Id
-    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = 1)
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     @Access(AccessType.PROPERTY)
     private Integer id;
