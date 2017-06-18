2c8e4a0f1f6d4e187ff3ab7704746ffeb5a18d53
ales84 <tolyaskrg@gmail.com>
2017-06-18 16:47:28 +0600

    Refactoring

--- src/test/java/org/alesapps/votingsystem/RestaurantTestData.java
+++ src/test/java/org/alesapps/votingsystem/RestaurantTestData.java
@@ -3,9 +3,6 @@ package org.alesapps.votingsystem;
 import org.alesapps.votingsystem.model.BaseEntity;
 import org.alesapps.votingsystem.model.Restaurant;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by Anatoliy Kozhayev on 16.06.2017.
  */
@@ -16,6 +13,4 @@ public class RestaurantTestData {
     public static final Restaurant RESTAURANT1 = new Restaurant(RESTAURANT1_ID, "Бочонок");
     public static final Restaurant RESTAURANT2 = new Restaurant(RESTAURANT1_ID + 1, "Форпост");
     public static final Restaurant RESTAURANT3 = new Restaurant(RESTAURANT1_ID + 2, "Рубин");
-
-    public static final List<Restaurant> RESTAURANTS = Arrays.asList(RESTAURANT1, RESTAURANT2, RESTAURANT3);
 }
